5b7b9f84b8ff9353f8d0da2c1f76b810fb321670
AspDotSina <sabetghadamsina04@gmail.comDear User@SINA>
2023-05-12 22:38:54 +0330

    Add project files.

---
+++ Strategy/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+Context context = new Context(new BMW());
+
+context.returnCar();
+
+
+public abstract class Car
+{
+    public abstract void BuildCar();
+}
+
+
+public class BMW : Car
+{
+    public override void BuildCar()
+    {
+        Console.WriteLine("Bmw ..........");
+    }
+
+
+}
+
+
+
+public class Benz : Car
+{
+    public override void BuildCar()
+    {
+        Console.WriteLine("Benz ..........");
+    }
+}
+
+
+
+
+public class Context
+{
+    private readonly Car _car;
+    public Context(Car car)
+    {
+        _car = car;
+    }
+
+
+    public void returnCar()
+    {
+        _car.BuildCar();
+    }
+}
